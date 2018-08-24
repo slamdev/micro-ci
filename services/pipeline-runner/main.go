4fe2db4b6e079ad72a4be6659b1eeabeeebf5f0c
Valentin Fedoskin <valentin.fedoskin@gmail.com>
2018-08-24 12:46:28 +1000

    clone remote repo, read pipeline file and create k8s job

--- services/pipeline-runner/main.go
+++ services/pipeline-runner/main.go
@@ -5,9 +5,17 @@ import (
 	"github.com/golang/protobuf/proto"
 	"github.com/nats-io/go-nats"
 	"github.com/slamdev/micro-ci/etc/schema"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	batch "k8s.io/api/batch/v1"
+	meta "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"time"
 )
 
@@ -34,7 +42,7 @@ func send() {
 	commit := &schema.Commit{
 		Author:   "slamdev",
 		Branch:   "master",
-		Revision: "123",
+		Revision: "758e01c07c119aeadb5dc31f7aadac69ece69ebd",
 	}
 	data, err := proto.Marshal(commit)
 	if err != nil {
@@ -65,11 +73,82 @@ func listen() {
 			log.Fatal(err)
 			return
 		}
-		log.Println(commit)
+		runPipeline(commit)
 	})
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 	log.Println(sub)
+}
+
+func runPipeline(commit *schema.Commit) {
+	repoUrl := "https://github.com/slamdev/micro-ci.git"
+	cloneDir := "build/clone"
+	execCommand("git clone --branch master "+repoUrl+" "+cloneDir, "")
+	execCommand("git checkout "+commit.Branch, cloneDir)
+	execCommand("git reset --hard "+commit.Revision, cloneDir)
+	execCommand("git merge --no-ff --no-commit origin/master", cloneDir)
+	content, err := ioutil.ReadFile(cloneDir + ".pipeline.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	pipeline := &Pipeline{}
+	err = yaml.UnmarshalStrict(content, pipeline)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, job := range pipeline.Jobs {
+		runJob(job)
+	}
+}
+
+func runJob(job Job) {
+	log.Println(job)
+	var config *rest.Config
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		kubeconfig := os.Getenv("HOME") + "/.kube/config"
+		config, err = clientcmd.BuildConfigFromFlags("", kubeconfig)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	jobTemplate := &batch.Job{
+		ObjectMeta: meta.ObjectMeta{
+			Name: "sample",
+		},
+		Spec: batch.JobSpec{},
+	}
+	log.Println(jobTemplate)
+	_, err = clientset.BatchV1().Jobs("micro-ci").Create(jobTemplate)
+	if err != nil {
+		log.Fatal(err)
+	}
+	jobList, err := clientset.BatchV1().Jobs("micro-ci").List(meta.ListOptions{})
+	log.Println(jobList.Items)
+}
+
+func execCommand(command string, dir string) {
+	cmd := exec.Command("sh", "-c", command)
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+type Pipeline struct {
+	Jobs []Job
+}
+
+type Job struct {
+	Name string
+	Spec map[interface{}]interface{}
 }
